9dd9170c84dfb812740d3e37a9eb3b80d085d054
Mark Force <force.j.mark@gmail.com>
2020-09-24 13:57:39 -0400

    test, get, getById completed

---
+++ README.md
@@ -0,0 +1,1 @@
+Bookmarks Server with routing, validation, and POST/DELETE/GET functionality completed by Mark F. and Marquel G.
---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks_test (title, url, rating, description)
+VALUES 
+('Google', 'https://google.com', '4', 'google some shit'),
+('Youtube', 'https://youtube.com', '4', 'google but searching videos and some shit'),
+('Thinkful', 'https://thinkful.com', '4', 'learn some shit');
